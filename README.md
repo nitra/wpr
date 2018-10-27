11b4e5b82bee7c3c48b551e7986b9c4f2b1b598e
vitaliytv <vitaliytv@nitralabs.com>
2018-10-27 17:18:54 +0300

    init

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # wpr
 Web Page Replay Go binary
+
+Platform dependent binary installer of [WebPageReplay](https://github.com/catapult-project/catapult/tree/master/web_page_replay_go) for node projects. Useful for serverless.
+
+Provide binary of `wpr` for the Linux x64 and Mac platforms.
+
+## Install
+
+    npm install --save wpr
+    
+## Usage examples
+
+```javascript
+const wpr = require('wpr')
+
+await wpr.startWpr()
+await wpr.stopWpr()
+```
---
+++ index.js
@@ -0,0 +1,58 @@
+'use strict'
+
+const path = require('path')
+const os = require('os')
+const platform = os.platform()
+const {
+  spawn
+} = require('child_process')
+const {
+  promisify
+} = require('util')
+
+const fs = require('fs')
+const deleteFileAsync = promisify(fs.unlink)
+
+const wprFile = '/tmp/archive.wprgo'
+let child
+
+exports.startWpr = async function () {
+  try {
+    const wprPath = path.resolve(__dirname, 'platforms')
+
+    child = spawn(`${wprPath}/${platform}/wpr`, ['replay', '--http_port=8080', '--https_port=8081', wprFile], {
+      cwd: wprPath
+    })
+
+    // Show WPR output
+    if (process.env.DEBUG) {
+      child.stderr.on('data', (data) => {
+        console.log(`wpr:\n${data}`)
+      })
+    }
+
+    console.log('Wpr started')
+  } catch (err) {
+    console.error(err)
+  }
+}
+
+exports.stopWpr = async function () {
+  try {
+    if (child !== undefined) {
+      console.log('Child exist')
+      child.kill('SIGINT')
+    }
+
+    try {
+      await deleteFileAsync(wprFile)
+      console.log(`${wprFile} deleted`)
+    } catch (err) {
+      console.log(`${wprFile} not exist`)
+    }
+
+    console.log('Wpr stopped and file cleaned')
+  } catch (err) {
+    console.error(err)
+  }
+}
